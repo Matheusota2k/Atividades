d26c2719e6ed6f56c0bed8a4a9755b4a1e9307c8
Matheus Farias Porto Dos Anjos <114027492+Matheusota2k@users.noreply.github.com>
2023-09-23 23:46:57 -0300

    Add files via upload

--- Banco de Dados/Sistema Bancario.sql
+++ Banco de Dados/Sistema Bancario.sql
@@ -59,8 +59,6 @@ insert into cliente(cpf, nome, sexo, endereco) values ('111.222.333-44', 'Matheu
 insert into cliente(cpf, nome, sexo, endereco) values ('666.777.888-99', 'Alvaro George', 'M', 'Av. Epitacio Pessoa, 1008');
 insert into cliente(cpf, nome, sexo, endereco) values ('555.444.777.33', 'Bruna Andrade', 'F', 'Rua Bancario Sergio Guerra, 640');
 
--- alter table cliente modify cpf char(15); Utilizado pra modificar o tamanho de caracteres do cpf.
-
 insert into conta(numero_conta, saldo, tipo_conta, num_agencia) values ('11765-2', '22.745.05', '2', '322');
 insert into conta(numero_conta, saldo, tipo_conta, num_agencia) values ('21010-7', '3.100.96', '1', '1253');
 
@@ -73,4 +71,44 @@ insert into telefone_cliente(cpf_cli, telefone) values ('666.777.888-99', '(83) 
 insert into telefone_cliente(cpf_cli, telefone) values ('666.777.888-99', '(83) 3233-2267');
 
 alter table cliente add column pais char(3) default('BRA');
--- alter table cliente add column email varchar(50);  Linha não executada.
+alter table cliente add column email varchar(50);
+
+delete from conta where numero_conta='11765-2';
+
+select * from agencia;
+
+update agencia set numero_agencia='6342' where cod_banco='4';
+
+select * from agencia;
+
+alter table conta modify numero_conta char(7);
+
+update cliente set email='matheus.farias@gmail.com' where cpf='111.222.333-44';
+
+select * from cliente;
+
+alter table conta add column desconto decimal;
+
+update conta set desconto = '10%' WHERE numero_conta='210107';
+
+select * from conta;
+
+update cliente set nome = 'Bruna Fernandez' where cpf = '555.444.777-33';
+
+update conta set tipo_conta = 3 where saldo >= 10.000;
+
+select nome, sexo from cliente order by nome desc;
+
+SELECT A.numero_agencia, SUM(C.saldo) AS saldo_total_agencia FROM agencia A LEFT JOIN conta C ON A.numero_agencia = C.num_agencia GROUP BY A.numero_agencia;
+
+SELECT A.numero_agencia, COUNT(C.cpf) AS quantidade_clientes FROM agencia A LEFT JOIN cliente C ON A.numero_agencia = C.num_agencia GROUP BY A.numero_agencia;
+
+SELECT C.nome AS NomeCliente, C.endereco AS EnderecoCliente, CO.numero_conta AS NumeroConta FROM cliente C INNER JOIN historico H ON C.cpf = H.cpf_cliente INNER JOIN conta CO ON H.num_conta = CO.numero_conta;
+
+SELECT sexo, COUNT(*) AS quantidade_pessoas FROM cliente GROUP BY sexo;
+
+SELECT C.nome AS NomeCliente FROM cliente C INNER JOIN historico H ON C.cpf = H.cpf_cliente INNER JOIN conta CO ON H.num_conta = CO.numero_conta ORDER BY CO.saldo DESC LIMIT 1;
+
+SELECT C.nome AS NomeCliente, CO.saldo AS SaldoOriginal, ROUND(CO.saldo * 1.035, 2) AS NovoSaldoComAumento FROM cliente C INNER JOIN historico H ON C.cpf = H.cpf_cliente INNER JOIN conta CO ON H.num_conta = CO.numero_conta;
+
+SELECT nome FROM cliente WHERE endereco  LIKE 'Av%';
